7085b0b020d4ebabfe6d37d96bc5a0aec93c92af
ly <rg14ly@163.com>
2020-02-11 15:23:58 +0800

    feature

---
+++ src/main/java/sjz/sgy/lb/LbApplication.java
@@ -0,0 +1,20 @@
+package sjz.sgy.lb;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.jms.annotation.EnableJms;
+
+@EnableJms
+@SpringBootApplication
+@ServletComponentScan
+@MapperScan("sjz.sgy.lb.dao")
+public class LbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbApplication.class, args);
+	}
+	
+}
+
---
+++ src/main/java/sjz/sgy/lb/activeMq/ActiveMqConstant.java
@@ -0,0 +1,8 @@
+package sjz.sgy.lb.activeMq;
+
+public class ActiveMqConstant {
+// 广播模式名称
+public static final String TOPIC_NANE = "topicName";
+// 队列模式名称
+public static final String QUEUE_NANE = "queueName";
+}
